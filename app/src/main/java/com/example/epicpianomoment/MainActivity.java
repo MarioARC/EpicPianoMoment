bd1814602888a7698090bae28c9917f4324845f2
91093467 <91093467@mvusd.net>
2021-10-14 08:46:08 -0700

    added sound to button and button overlay

--- app/src/main/java/com/example/epicpianomoment/MainActivity.java
+++ app/src/main/java/com/example/epicpianomoment/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.epicpianomoment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void click(View v)
+    {
+        MediaPlayer mp = MediaPlayer.create(this, R.raw.samsung);
+        mp.start();
+    }
+
 }
