a249a3b9add2580b812333b8e5d604d963f70eba
91093467 <91093467@mvusd.net>
2021-10-28 08:31:01 -0700

    added spinner dropdown

--- app/src/main/java/com/example/epicpianomoment/MainActivity.java
+++ app/src/main/java/com/example/epicpianomoment/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.epicpianomoment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
