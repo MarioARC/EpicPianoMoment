6e36bbe8cc331b6bac477f507e724ce25aaedbdf
91093467 <91093467@mvusd.net>
2021-10-28 08:38:39 -0700

    added items to spinner

--- app/src/main/java/com/example/epicpianomoment/MainActivity.java
+++ app/src/main/java/com/example/epicpianomoment/MainActivity.java
@@ -13,6 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner dropdown = findViewById(R.id.spinner);
+        String[] items = new String[]{"Octave 1", "Octave 2", "Octave 3", "Octave 4", "Octave 5", "Octave 6", "Octave 7"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
     }
 
 //    public void click(View v)
