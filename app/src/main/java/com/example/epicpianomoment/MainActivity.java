1e4526878e8f7f536833e7dbe64648e3b88bff06
91093467 <91093467@mvusd.net>
2021-10-28 08:20:39 -0700

    finished adding buttons to octaves 0 and 1

--- app/src/main/java/com/example/epicpianomoment/MainActivity.java
+++ app/src/main/java/com/example/epicpianomoment/MainActivity.java
@@ -2,10 +2,7 @@ package com.example.epicpianomoment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
