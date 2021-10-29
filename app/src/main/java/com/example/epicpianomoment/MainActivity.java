137b18fbbe98cf09d60c10be640797d7f8ad3094
91093467 <91093467@mvusd.net>
2021-10-29 08:55:33 -0700

    added second activity and working dropdown

--- app/src/main/java/com/example/epicpianomoment/MainActivity.java
+++ app/src/main/java/com/example/epicpianomoment/MainActivity.java
@@ -2,22 +2,49 @@ package com.example.epicpianomoment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private Spinner dropdown;
+    private static final String[] items = new String[]{"Octave 1", "Octave 2", "Octave 3", "Octave 4", "Octave 5", "Octave 6", "Octave 7"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Spinner dropdown = findViewById(R.id.spinner);
-        String[] items = new String[]{"Octave 1", "Octave 2", "Octave 3", "Octave 4", "Octave 5", "Octave 6", "Octave 7"};
+        dropdown = findViewById(R.id.spinner);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
         dropdown.setAdapter(adapter);
+        dropdown.setOnItemSelectedListener(this);
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View v, int pos, long id)
+    {
+        switch(pos)
+        {
+            case 0:
+                break;
+            case 1:
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+                break;
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+        }
+    }
+
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 
 //    public void click(View v)
