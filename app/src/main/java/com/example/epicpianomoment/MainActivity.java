c37500929e1b6eb42bc4a731d51a0fcf04f662ea
MarDi <MarDiamond10@outlook.com>
2021-10-31 09:58:55 -0700

    added sound to o0keyA

--- app/src/main/java/com/example/epicpianomoment/MainActivity.java
+++ app/src/main/java/com/example/epicpianomoment/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.epicpianomoment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -63,6 +64,12 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     public void onNothingSelected(AdapterView<?> adapterView) {
 
+    }
+
+    public void click0a(View v)
+    {
+        MediaPlayer mp = MediaPlayer.create(this, R.raw.zeroa);
+        mp.start();
     }
 
 //    public void click(View v)
